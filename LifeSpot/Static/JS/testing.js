715a722e810e8c0b850c12c364581a1784250550
Almeta-SPO@outlook.com <Almeta-SPO@outlook.com>
2024-11-14 12:58:31 +0300

    футер зафиксирован

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,4 @@
+﻿//получим коллекцию всех элементов страницы
+let elements = document.getElementsByTagName('*');
+//выведем результат в уведомлении
+alert(`Количество элементов на странице: ${elements.length}`);
