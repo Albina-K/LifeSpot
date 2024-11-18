5a43b699fd5c3eafcdbe346a8f7ac28e3ff63c03
Almeta-SPO@outlook.com <Almeta-SPO@outlook.com>
2024-11-18 15:04:31 +0300

    пройден модуль 29

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,28 @@
+﻿function getReview() {
+    let review = {}
+
+    review["userName"] = prompt("Как Вас зовут?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    review["date"] = new Date().toLocaleString()
+
+    writeReview(review)
+}
+
+/*
+* Запишем отзыв на страницу
+*
+* */
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,59 +1,71 @@
-﻿//создадим объект Мар для хранения сессии
+﻿/*
+* Сессия теперь создается в общей области видимости.
+* Будет "захватываться" тремя функциями
+* 
+* */ 
 let session = new Map();
-//Сохраним UserAgent
-session.set("userAgent", window.navigator.userAgent)
 
-// Запросим возраст пользователя и сохраним в переменную
-session.set("age", prompt("Пожалуйста, введите ваш возраст"))
-
-// Проверка на возраст и сохранение сессии
-if (session.get("get") >= 18) {
-    let startDate = new Date().toLocaleString();
-    // Те, кто старше 18, увидят приветствие и будут направлены на сайт
-    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время:" + startDate);
-    session.set("startDate", startDate)
-}
-else {
-    // Если введенное число < 18, либо если введено не число -
-    // пользователь будет перенаправлен
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com"
-    a = true + 20 + "name"
+/*
+* Сохранение данных сессии сразу при заходе пользователя на страницу
+* 
+* */
+function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    //Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
 }
 
-// Вывод в консоль
-for (let result of session) {console.log(result) }
+/*
+* Проверка возраста пользователя
+* 
+* */
 
-let userName = prompt("Пожалуйста, введите имя");
-alert(`Приветсвуем, ${userName}. В имени ${userName.length} символов`)
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        // Те, кто старше 18, увидят приветствие и будут направлены на сайт
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время:" + new Date().toLocaleString());
+        }
+    else {
+        // Если введенное число < 18, либо если введено не число -
+        // пользователь будет перенаправлен
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+        // a = true + 20 + "name"
+    }
+}
+
+    // Вывод в консоль
+let sessionLog = function () {
+    for (let result of session) { console.log(result) }
+}
+
+//let userName = prompt("Пожалуйста, введите имя");
+//alert(`Приветсвуем, ${userName}. В имени ${userName.length} символов`)
 
 function filterContent() {
     /*Код ниже отрабатывает каждый раз при вводе символа в input*/
     // Сохраняем текст пользовательского запроса.
-    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+  
     // Находим контейнеры с видео, которые необходимо фильтровать
     let elements = document.getElementsByClassName('video-container');
 
     // Пробежимся в цикле по контейнерам
     for (let i = 0; i <= elements.length; i++) {
-
-        //// Получим всё что внутри контейнера
-        //let childElements = elements[i];
-        //// Получим элемент, содержащий описание видео
-        //// Он у нас единственный с тегом h3, снова воспользуемся поиском по тегу,
-        //let videoDescription = childElements.getElementsByTagName('h3')[0];
-        //// Выведем его текст на консоль
-        //console.log(videoDescription.innerText);
-
-
         // Вытаскиваем текст описания видео, которое необходимо отфильтровать
-        let videoText = elements[i].querySelector(".video-title").innerText;
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
         // Вытаскиваем текст описания видео, которое необходимо отфильтровать
-        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {//Описание
+        if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())) {//скрываем неподходящие
             elements[i].style.display = 'none';
         }
         else {
-            elements[i].style.display = 'inline-blok';
+            //показываем подходящие
+            elements[i].style.display = 'inline-bloсk';
         }
     }
 }
+
+
